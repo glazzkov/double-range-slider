e224340f13608f091cc0ea2d096b1256194d56cd
= <alexanderglazzkov@gmail.com>
2023-01-20 07:40:20 +0700

    Полный рефакторинг скрипта

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,8 @@
-import { initRange } from './range.js';
+import { range } from './range.js';
 
 
-initRange('#range-wrapper', 50, 200);
+const range1 = range('#range-wrapper', 80, 500, 200, 450);
+const range2 = range('#range-wrapper1', 2454, 4325643, 555600, 3645326);
+
+range1.init();
+range2.init();
--- source/js/range.js
+++ source/js/range.js
@@ -1,194 +1,179 @@
-// DOM-элементы
-let rangeWrapper;
-let rangeBar;
-let rangeScale;
-let rangeToggleMin;
-let rangeToggleMax;
-let inputMin;
-let inputMax;
-// переменные для управления координатами ползунков
-let cursorPositionX;
-let currentToggle;
-let scaleMarginLeft;
-let scaleMarginRight;
-let barWidth;
-let deltaPositionX;
-let deltaPositionPercent;
+export const range = (rangeSelector = '#range-wrapper', minValue = 100, maxValue = 1000, minDefault = minValue, maxDefault = maxValue) => {
+  // объект слайдера, возвращаемый функцией
+  const current = {
+    // инициализация слайдера
+    init: () => {
+      // DOM-элементы
+      current.wrapper = document.querySelector(rangeSelector);
+      current.bar = current.wrapper.querySelector('.range__bar');
+      current.scale = current.wrapper.querySelector('.range__scale');
+      current.toggleRight = current.wrapper.querySelector('.range__toggle--min');
+      current.toggleLeft = current.wrapper.querySelector('.range__toggle--max');
+      current.inputMin = current.wrapper.querySelector('.range__input--min');
+      current.inputMax = current.wrapper.querySelector('.range__input--max');
+      current.currentToggle = null;
+      // переменные для движения toggle
+      current.scaleMarginLeft = parseInt(current.scale.style.marginLeft);
+      current.scaleMarginRight = parseInt(current.scale.style.marginRight);
+      current.mouseX = null;
+      current.barWidth = current.bar.offsetWidth;
+      // переменные инпутов
+      current.rangeMinValue = minValue;
+      current.rangeMaxValue = maxValue;
+      current.rangeMinDefault = minDefault;
+      current.rangeMaxDefault = maxDefault;
+      current.setDefaultValues();
 
-// переменные значений полей инпутов
-let rangeMinValue;
-let rangeMaxValue;
+      current.toggleRight.addEventListener('mousedown', current.downEvent);
+      current.toggleLeft.addEventListener('mousedown', current.downEvent);
+      current.inputMin.addEventListener('change', current.changeEvent);
+      current.inputMax.addEventListener('change', current.changeEvent);
+    },
 
-// булевые функции для проверки изменения координат
-const isMarginLeftChangable = (deltaMargin) => {
-  let maxMargin = 95 - scaleMarginRight;
-  let minMargin = 0.1;
-  let newMargin = scaleMarginLeft + deltaMargin;
-  if ((newMargin <= minMargin) || (newMargin >= maxMargin)) {
-    return false;
-  }
-  return true;
-}
+    // событие нажатия мыши
+    downEvent: (evt) => {
+      current.currentToggle = evt.target;
+      current.mouseX = evt.pageX;
+      current.barWidth = current.bar.offsetWidth;
+      window.addEventListener('mousemove', current.moveEvent);
+      window.addEventListener('mouseup', current.upEvent)
+    },
 
-const isMarginRightChangable = (deltaMargin) => {
-  let maxMargin = 95 - scaleMarginLeft;
-  let minMargin = 0.1;
-  let newMargin = scaleMarginRight - deltaMargin;
-  if ((newMargin <= minMargin) || (newMargin >= maxMargin)) {
-    return false;
-  }
-  return true;
-}
+    // событие движения мыши при зажатой кнопке
+    moveEvent: (evt) => {
+      current.barWidth = current.bar.offsetWidth;
+      let deltaX = current.deltaX(current.mouseX, evt.pageX);
+      let deltaXPercent = current.deltaXPercent(current.barWidth, deltaX);
+      current.changeMargin(deltaXPercent);
+      current.mouseX = evt.pageX;
+    },
 
-const isCurrentToggleMin = () => {
-  if (currentToggle.classList.contains('range__toggle--min')) {
-    return true;
-  }
-  return false;
-}
+    // событие отжатия мыши, удаляет событие движения
+    upEvent: (evt) => {
+      window.removeEventListener('mousemove', current.moveEvent);
+    },
 
-const isCurrentToggleMax = () => {
-  if (currentToggle.classList.contains('range__toggle--max')) {
-    return true;
-  }
-  return false;
-}
-
-const isInputsExist = () =>{
-  if (inputMin && inputMax) {
-    return true
-  }
-  return false;
-}
-
-// меняет минимальное значение в инпуте при изменении левого toggle
-const changeMinValue = () => {
-  let marginInPixelx = barWidth() / 100 * parseInt(scaleMarginLeft);
-  let value = rangeMinValue + Math.round(marginInPixelx * (rangeMaxValue - rangeMinValue) / barWidth());
-  inputMin.value = value;
-}
-
-// меняет максимальное значение в инпуте при изменении правого toggle
-const changeMaxValue = () => {
-  let marginInPixelx = barWidth() / 100 * parseInt(scaleMarginRight);
-  let value = rangeMaxValue - Math.round(marginInPixelx * (rangeMaxValue - rangeMinValue) / barWidth());
-  inputMax.value = value;
-}
-// перемещает левый toggle при изменении значения в инпуте
-const changeMarginLeft = () => {
-  let newMax = rangeMaxValue - rangeMinValue;
-  let newValue = inputMin.value - rangeMinValue;
-  let margin = (newValue / newMax) * 100;
-  let maxMargin = 95 - scaleMarginRight;
-  let minMargin = 0.1;
-  if (margin < minMargin) {
-    margin = minMargin;
-  } else if (margin > maxMargin) {
-    margin = maxMargin;
-  }
-  scaleMarginLeft = margin;
-  rangeScale.style.marginLeft = margin + '%';
-}
-
-// перемещает правый toggle при изменении значения в инпуте
-const changeMarginRight = () => {
-  let newMax = rangeMaxValue - rangeMinValue;
-  let newValue = (inputMax.value - rangeMinValue);
-  let margin = ((newMax - newValue) / newMax) * 100;
-  let maxMargin = 95 - scaleMarginLeft;
-  let minMargin = 0.1;
-  if (margin < minMargin) {
-    margin = minMargin;
-  } else if (margin > maxMargin) {
-    margin = maxMargin;
-  }
-  scaleMarginRight = margin;
-  rangeScale.style.marginRight = margin + '%';
-}
-
-//
-const range = {
-  // инициализация range
-  init: (selector, minValue, maxValue, minDefault, maxDefault) => {
-    rangeWrapper = () => document.querySelector(selector);
-    rangeBar = rangeWrapper().querySelector('.range__bar');
-    rangeScale = rangeWrapper().querySelector('.range__scale');
-    rangeToggleMin = rangeWrapper().querySelector('.range__toggle--min');
-    rangeToggleMax = rangeWrapper().querySelector('.range__toggle--max');
-    inputMin = rangeWrapper().querySelector('.range__input--min');
-    inputMax = rangeWrapper().querySelector('.range__input--max');
-    scaleMarginLeft = parseInt(rangeScale.style.marginLeft);
-    scaleMarginRight = parseInt(rangeScale.style.marginRight);
-
-    barWidth = () => rangeBar.offsetWidth;
-    deltaPositionX = (xStart, xEnd) => xStart - xEnd;
-    deltaPositionPercent = (evt) => (deltaPositionX(evt.pageX, cursorPositionX) / barWidth() * 100);
-
-    rangeMinValue = minValue;
-    rangeMaxValue = maxValue;
-    if (isInputsExist()) {
-      inputMin.value = minDefault;
-      inputMax.value = maxDefault;
-      changeMarginLeft();
-      changeMarginRight();
-      inputMin.addEventListener('change', range.valueChanged);
-      inputMax.addEventListener('change', range.valueChanged);
-    }
-
-    rangeToggleMin.addEventListener('mousedown', range.mouseDown);
-    rangeToggleMax.addEventListener('mousedown', range.mouseDown);
-  },
-
-  // обработчик нажатия кнопки мыши
-  mouseDown: (evt) => {
-    cursorPositionX = evt.pageX;
-    currentToggle = evt.target;
-    window.addEventListener('mousemove', range.mouseMove);
-    window.addEventListener('mouseup', range.mouseUp);
-  },
-
-  // обработчик движения мыши при зажатой кнопке
-  mouseMove: (evt) => {
-    let deltaX = deltaPositionPercent(evt);
-    if (isCurrentToggleMin() && isMarginLeftChangable(deltaX)) {
-      scaleMarginLeft += deltaX;
-      rangeScale.style.marginLeft = (scaleMarginLeft) + '%';
-      if (isInputsExist()) {
-        changeMinValue();
+    changeEvent: (evt) => {
+      let input = evt.target;
+      if (current.isInputMin(input)) {
+        current.changeMarginLeft();
+      } else if (current.isInputMax(input)) {
+        current.changeMarginRight();
       }
-    } else if (isCurrentToggleMax() && isMarginRightChangable(deltaX)) {
-      scaleMarginRight -= deltaX;
-      rangeScale.style.marginRight = (scaleMarginRight) + '%';
-      if (isInputsExist()) {
-        changeMaxValue();
+    },
+
+    // меняет margin scale, сдвигает toggle
+    changeMargin: (deltaMargin) => {
+      const toggle = current.currentToggle;
+      const scale = current.scale;
+      if (current.isToggleLeft(toggle) && current.isMarginChangable(deltaMargin)) {
+        current.scaleMarginLeft += deltaMargin;
+        scale.style.marginLeft = `${current.scaleMarginLeft}%`;
+        current.changeMinValue();
+      } else if (current.isToggleRight(toggle) && current.isMarginChangable(deltaMargin)) {
+        current.scaleMarginRight -= deltaMargin;
+        scale.style.marginRight = `${current.scaleMarginRight}%`;
+        current.changeMaxValue();
       }
-    }
-    cursorPositionX = evt.pageX;
-  },
+    },
 
-  // удаляет обработчик движения мыши после отжатия клика
-  mouseUp: (evt) => {
-    window.removeEventListener('mousemove', range.mouseMove);
-  },
-  // обработчик изменения в инпутах
-  valueChanged: (evt) => {
-    if (evt.target.classList.contains('range__input--min')) {
-      changeMarginLeft();
-    } else if (evt.target.classList.contains('range__input--max')) {
-      changeMarginRight();
-    }
+    // перемещает toggle при изменении значений в левом инпуте
+    changeMarginLeft: () => {
+      let newMax = current.rangeMaxValue - current.rangeMinValue;
+      let newValue = current.inputMin.value - current.rangeMinValue;
+      let margin = (newValue / newMax) * 100;
+      let maxMargin = 95 - current.scaleMarginRight;
+      let minMargin = 0.1;
+      if (margin < minMargin) {
+        margin = minMargin;
+      } else if (margin > maxMargin) {
+        margin = maxMargin;
+      }
+      current.scaleMarginLeft = margin;
+      current.scale.style.marginLeft = margin + '%';
+    },
 
-  }
+    // перемещает toggle при изменении значений в правом инпуте
+    changeMarginRight: () => {
+      let newMax = current.rangeMaxValue - current.rangeMinValue;
+      let newValue = (current.inputMax.value - current.rangeMinValue);
+      let margin = ((newMax - newValue) / newMax) * 100;
+      let maxMargin = 95 - current.scaleMarginLeft;
+      let minMargin = 0.1;
+      if (margin < minMargin) {
+        margin = minMargin;
+      } else if (margin > maxMargin) {
+        margin = maxMargin;
+      }
+      current.scaleMarginRight = margin;
+      current.scale.style.marginRight = margin + '%';
+    },
+
+    // меняет минимальное значение range в инпуте
+    changeMinValue: () => {
+      if (current.isInputsExist()) {
+        let marginInPixelx = current.barWidth / 100 * parseInt(current.scaleMarginLeft);
+        let min = current.rangeMinValue;
+        let max = current.rangeMaxValue
+        let value = min + Math.round(marginInPixelx * (max - min) / current.barWidth);
+        current.inputMin.value = value;
+      }
+    },
+
+    // меняет максимальное значение range в инпуте
+    changeMaxValue: () => {
+      if (current.isInputsExist()) {
+        let marginInPixelx = current.barWidth / 100 * parseInt(current.scaleMarginRight);
+        let min = current.rangeMinValue;
+        let max = current.rangeMaxValue
+        let value = max - Math.round(marginInPixelx * (max - min) / current.barWidth);
+        current.inputMax.value = value;
+      }
+    },
+
+    // валидирует минимальные и максимальные значения
+    validateValues: () => {
+      if (current.rangeMinValue > current.rangeMaxValue) {
+        [current.rangeMinValue, current.rangeMaxValue] = [current.rangeMaxValue, current.rangeMinValue];
+      }
+      if (current.rangeMinDefault > current.rangeMaxDefault) {
+        [current.rangeMinDefault, current.rangeMaxDefault] = [current.rangeMaxDefault, current.rangeMinDefault];
+      }
+    },
+    setDefaultValues: () => {
+      if (current.isInputsExist()) {
+        current.validateValues();
+        current.inputMin.value = current.rangeMinDefault;
+        current.inputMax.value = current.rangeMaxDefault;
+        current.changeMarginLeft();
+        current.changeMarginRight();
+      }
+    },
+
+
+    //проверяет, может ли быть изменен margin
+    isMarginChangable: (deltaMargin) => {
+      let minMargin = 0.1;
+      if (current.isToggleLeft(current.currentToggle)) {
+        let maxMargin = 95 - current.scaleMarginRight;
+        let newMargin = current.scaleMarginLeft + deltaMargin;
+        return (newMargin >= minMargin) && (newMargin <= maxMargin);
+      } else if (current.isToggleRight(current.currentToggle)) {
+        let maxMargin = 95 - current.scaleMarginLeft;
+        let newMargin = current.scaleMarginRight - deltaMargin;
+        return (newMargin >= minMargin) && (newMargin <= maxMargin);
+      }
+    },
+
+    isToggleLeft: (toggle) => toggle.classList.contains('range__toggle--min'),
+    isToggleRight: (toggle) => toggle.classList.contains('range__toggle--max'),
+    isInputMin: (input) => input.classList.contains('range__input--min'),
+    isInputMax: (input) => input.classList.contains('range__input--max'),
+    isInputsExist: () => (current.inputMin && current.inputMax) ? true : false,
+
+    deltaX: (xBefore, xAfter) => xAfter - xBefore,
+    deltaXPercent: (barWidth, deltaX) => (deltaX / barWidth) * 100,
+
+  };
+  return current;
 }
-
-const initRange = (selector = '#range-wrapper', minValue = 0, maxValue = 1000, minDefault = minValue, maxDefault = maxValue) => {
-  if (minValue > maxValue) {
-    [minValue, maxValue] = [maxValue, minValue]
-  }
-  if (minDefault > maxDefault) {
-    [minDefault, maxDefault] = [maxDefault, minDefault];
-  }
-  range.init(selector, minValue, maxValue, minDefault, maxDefault);
-}
-
-export { initRange };
